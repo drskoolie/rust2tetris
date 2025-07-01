51605e129f023647caed25da46913559b1b1c635
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-01 08:17:37 -0400

    Added a bit of code to main to remove annoying warnings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ mod alu;
 mod gates;
 mod sequential;
 
+use sequential::Register;
+use alu::alu;
+
 fn main() {
-    println!("Hello, world!");
+    let mut reg = Register::new();
+    let (output, _zr, _ng) = alu(
+        0x1,
+        0x1,
+        false,
+        false,
+        false,
+        false,
+        true,
+        false);
+
+    reg.set_input(0xAAAA, true);
+    reg.tick();
+    println!("Output: {:016b}", reg.get_output());
+    println!("Output: {:016b}", output);
 }
