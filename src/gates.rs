cb50d83c74511405460156e732d7218f2c58ba04
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-01 05:30:33 -0400

    Not gate added

--- src/gates.rs
+++ src/gates.rs
@@ -2,6 +2,10 @@ pub fn nand16(a: u16, b: u16) -> u16 {
     !(a & b)
 }
 
+pub fn not16(a: u16) -> u16 {
+    nand16(a, a)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -11,5 +15,11 @@ mod tests {
         assert_eq!(nand16(0xFFFF, 0xFFFF), 0x0000);
         assert_eq!(nand16(0x0000, 0x0000), 0xFFFF);
         assert_eq!(nand16(0xFFFF, 0x0000), 0xFFFF);
+    }
+
+    #[test]
+    fn fest_not16() {
+        assert_eq!(not16(0x0000), 0xFFFF);
+        assert_eq!(not16(0xFFFF), 0x0000);
     }
 }
