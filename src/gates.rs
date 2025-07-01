0e0f239703216654f27da897716e874e4d9d6543
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-01 06:21:26 -0400

    Finished full adder

--- src/gates.rs
+++ src/gates.rs
@@ -24,6 +24,13 @@ pub fn half_adder(a: bool, b: bool) -> (bool, bool) {
     (sum, carry)
 }
 
+pub fn full_adder(a: bool, b: bool, carry_in: bool) -> (bool, bool) {
+    let (sum1, carry1) = half_adder(a, b);
+    let (sum2, carry2) = half_adder(sum1, carry_in);
+    let carry_out = carry1 || carry2;
+    (sum2, carry_out)
+}
+
 pub fn add16(a: u16, b:u16) -> u16 {
     xor16(a, b)
 }
@@ -71,5 +78,17 @@ mod tests {
         assert_eq!(half_adder(false, true), (true, false));
         assert_eq!(half_adder(true, false), (true, false));
         assert_eq!(half_adder(true, true), (false, true));
+    }
+
+    #[test]
+    fn test_full_adder() {
+        assert_eq!(full_adder(false, false, false), (false, false));
+        assert_eq!(full_adder(false, false, true), (true, false));
+        assert_eq!(full_adder(false, true, false), (true, false));
+        assert_eq!(full_adder(false, true, true), (false, true));
+        assert_eq!(full_adder(true, false, false), (true, false));
+        assert_eq!(full_adder(true, false, true), (false, true));
+        assert_eq!(full_adder(true, true, false), (false, true));
+        assert_eq!(full_adder(true, true, true), (true, true));
     }
 }
