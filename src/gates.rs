92a2626ddbdf75b79a34125d4347972478b475a9
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-01 05:42:52 -0400

    Add or

--- src/gates.rs
+++ src/gates.rs
@@ -6,6 +6,14 @@ pub fn not16(a: u16) -> u16 {
     nand16(a, a)
 }
 
+pub fn and16(a: u16, b: u16) -> u16 {
+    not16(nand16(a, b))
+}
+
+pub fn or16(a: u16, b: u16) -> u16 {
+    nand16(nand16(a, a), nand16(b, b))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,8 +26,18 @@ mod tests {
     }
 
     #[test]
-    fn fest_not16() {
+    fn test_not16() {
         assert_eq!(not16(0x0000), 0xFFFF);
         assert_eq!(not16(0xFFFF), 0x0000);
+    }
+
+    #[test]
+    fn test_and16() {
+        assert_eq!(and16(0b0011, 0b0101), 0b0001);
+    }
+
+    #[test]
+    fn test_or16() {
+        assert_eq!(or16(0b0011, 0b0101), 0b0111);
     }
 }
