16a1f018c24b2d27ecceaac06bfed6144cd3dae1
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-01 06:06:32 -0400

    Made half adder

--- src/gates.rs
+++ src/gates.rs
@@ -16,7 +16,16 @@ pub fn or16(a: u16, b: u16) -> u16 {
 
 pub fn xor16(a: u16, b: u16) -> u16 {
     nand16(nand16(nand16(a, b), a), nand16(nand16(a, b), b))
+}
 
+pub fn half_adder(a: bool, b: bool) -> (bool, bool) {
+    let sum = a ^ b;
+    let carry = a & b;
+    (sum, carry)
+}
+
+pub fn add16(a: u16, b:u16) -> u16 {
+    xor16(a, b)
 }
 
 #[cfg(test)]
@@ -51,4 +60,16 @@ mod tests {
         assert_eq!(xor16(0b0011, 0b0101), 0b0110);
     }
 
+    #[test]
+    fn test_add16() {
+        assert_eq!(add16(0b0001, 0b0001), 0b0010);
+    }
+
+    #[test]
+    fn test_half_adder() {
+        assert_eq!(half_adder(false, false), (false, false));
+        assert_eq!(half_adder(false, true), (true, false));
+        assert_eq!(half_adder(true, false), (true, false));
+        assert_eq!(half_adder(true, true), (false, true));
+    }
 }
