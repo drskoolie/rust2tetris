014db1becc1ee8a7e9fa1e58fa02a7e09d81bb7c
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-04 22:57:58 -0400

    Add stack to main

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,15 @@ mod vm;
 
 use crate::hardware::cpu::Cpu;
 use crate::parser::assembly::Assembler;
+use crate::vm::vm::Stack;
 
 fn main() {
     let mut cpu = Cpu::new();
     let mut asm = Assembler::new();
-    let source = r#"
-        @i
-        M=1
-        (LOOP)
-        @i
-        D=M
-        @100
-        D=D-A
-        @END
-        D;JGT
-        @LOOP
-        0;JMP
-        (END)
-        @END
-        0;JMP
-    "#;
-    asm.assemble_all(source);
+    let mut stack = Stack::new();
+
+    stack.push_value(10);
+    asm.assemble_all(&stack.commands.join("\n"));
 
     cpu.reset_pc();
     cpu.load_from_string(&asm.binaries.join("\n"));
