8b67100663c1556f5c972d2482826d4a99cdece9
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-07-05 13:05:41 -0400

    Added test for this

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1014,4 +1014,46 @@ mod tests {
     }
 
 
+    #[test]
+    fn test_pop_this() {
+        let mut cpu = Cpu::new();
+        let mut asm = Assembler::new();
+        let mut stack = Stack::new();
+
+        stack.push_command("constant", "25");
+        stack.pop_command("this", "0");
+        asm.assemble_all(&stack.assembly.join("\n"));
+        let no_of_instructions = asm.binaries.len();
+        cpu.load_from_string(&asm.binaries.join("\n"));
+        for _ in 0..no_of_instructions {
+            cpu.clock();
+        }
+
+        assert_eq!(256, cpu.get_data(0));
+        assert_eq!(25, cpu.get_data(3000));
+    }
+
+    #[test]
+    fn test_pop_this_index() {
+        let mut cpu = Cpu::new();
+        let mut asm = Assembler::new();
+        let mut stack = Stack::new();
+
+        stack.push_command("constant", "25");
+        stack.push_command("constant", "18");
+        stack.pop_command("this", "1");
+        stack.pop_command("this", "10");
+        asm.assemble_all(&stack.assembly.join("\n"));
+        let no_of_instructions = asm.binaries.len();
+        cpu.load_from_string(&asm.binaries.join("\n"));
+        for _ in 0..no_of_instructions {
+            cpu.clock();
+        }
+
+        assert_eq!(256, cpu.get_data(0));
+        assert_eq!(18, cpu.get_data(3000 + 1));
+        assert_eq!(25, cpu.get_data(3000 + 10));
+    }
+
+
 }
